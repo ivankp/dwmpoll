4b848f5900e878644b4911d42febeea61210c7cd
ivankp <ivan.pogrebnyak@gmail.com>
2021-07-14 21:52:55 -0400

    mem with free

--- dwmpoll.c
+++ dwmpoll.c
@@ -23,37 +23,41 @@ static void errptr(const char* fcn_name, int line) {
 }
 #define ERR(FCN_NAME) errptr(FCN_NAME,__LINE__)
 
-static int read_file(const char* fname, char* buff, int n) {
+static int read_file(const char* fname, char* buf, int n) {
   const int fd = open(fname, O_RDONLY);
   if (fd == -1) {
     ERR("open");
     return -1;
   }
-  ssize_t nread = read(fd,buff,n);
+  ssize_t nread = read(fd,buf,n);
   if (nread < 0) {
     ERR("read");
     return -1;
   }
-  buff[nread] = '\0';
+  buf[nread] = '\0';
   close(fd);
   return 0;
 }
-
-int epoll, minute_timer, inot;
-
-char status_text[256],
-  bat_capacity_text[8],
-  bat_status_text[8],
-  brightness_text[16],
-  time_text[64];
 
 static Display *dpy;
 static int screen;
 static Window root;
 
+int epoll, timer_1min, inot;
+
+char status_text[256],
+  cpu_text[16],
+  mem_text[16],
+  brightness_text[16],
+  bat_status_text[8],
+  bat_capacity_text[8],
+  time_text[64];
+
 void setroot(void) {
   sprintf(status_text,
-    " %s │ %s%s │ %s "
+    " %s │ %s │ %s │ %s%s │ %s "
+    , cpu_text
+    , mem_text
     , brightness_text
     , bat_status_text
     , bat_capacity_text
@@ -62,6 +66,75 @@ void setroot(void) {
   printf("%s\n",status_text);
   XStoreName(dpy,root,status_text);
   XFlush(dpy);
+}
+
+/* int cpu_sum = 0, cpu_idle, cpu_sum_prev = 0, cpu_idle_prev = 0; */
+void fmt_cpu(void) {
+  /*
+  const int fd = open("/proc/stat", O_RDONLY);
+  if (fd == -1) {
+    ERR("open");
+    return;
+  }
+  char buf[128];
+  ssize_t nread = read(fd,buf,sizeof(buf));
+  if (nread < 0) {
+    ERR("read");
+    return;
+  }
+  buf[nread] = '\0';
+  close(fd);
+
+  cpu_sum = 0;
+  int i = 0;
+  char* token = strtok(buf," ");
+  while (token) {
+    token = strtok(NULL," ");
+    if (token) {
+      cpu_sum += atoi(token);
+      if (i==3)
+        cpu_idle = atoi(token);
+      ++i;
+    }
+  }
+  snprintf(cpu_text,sizeof(cpu_text), "CPU%3.0f%%",
+    100-(cpu_idle-cpu_idle_prev)*100./(cpu_sum-cpu_sum_prev));
+  cpu_idle_prev = cpu_idle;
+  cpu_sum_prev = cpu_sum;
+  */
+  snprintf(cpu_text,sizeof(cpu_text), "CPU ???");
+}
+
+void fmt_mem(void) {
+  static char buf[128];
+  FILE* pipe = popen("free","r");
+  if (!pipe) {
+    ERR("popen");
+    return;
+  }
+  while (fgets(buf,sizeof(buf),pipe)) {
+    if (!strncmp(buf,"Mem:",4)) {
+      double total=-1, available=-1;
+      const char* token = strtok(buf," ");
+      for (int i=0;;++i) {
+        if (!(token = strtok(NULL," "))) break;
+        if (i==0) {
+          total = atoi(token);
+        } else if (i==5) {
+          available = atoi(token);
+          break;
+        }
+      }
+      if (available>=0) {
+        snprintf(mem_text,sizeof(mem_text), "MEM%3.0f%%",
+          100.*(1.-available/total));
+      } else {
+        snprintf(mem_text,sizeof(mem_text), "MEM ???");
+      }
+      break;
+    }
+  }
+  pclose(pipe);
 }
 
 struct itimerspec itspec;
@@ -134,7 +207,6 @@ void fmt_brightness(struct file_list_entry* f) {
 }
 
 static struct file_list_entry files[] = {
-  /* { 0, "/home/ivanp/test", battery } */
   { 0, "/sys/class/power_supply/BAT0/capacity", fmt_bat_capacity },
   { 0, "/sys/class/power_supply/BAT0/status", fmt_bat_status },
   { 0, "/sys/class/backlight/intel_backlight/brightness", fmt_brightness }
@@ -144,13 +216,19 @@ void epoll_loop() {
   struct epoll_event e;
   char inotify_buffer[sizeof(struct inotify_event)+64];
   for (;;) {
-    int n = epoll_wait(epoll, &e, 1, -1);
-    if (n < 0) ERR("epoll_wait");
-    else while (n--) {
+    int n = epoll_wait(epoll, &e, 1, 2000/*ms*/);
+    if (n < 0) {
+      ERR("epoll_wait");
+    } else if (n==0) {
+      fmt_cpu();
+      fmt_mem();
+      setroot();
+
+    } else while (n--) {
       /* uint32_t flags = e.events; */
       const int fd = e.data.fd;
 
-      if (fd == minute_timer) {
+      if (fd == timer_1min) {
         long int timersElapsed = 0;
         (void)read(fd, &timersElapsed, sizeof(timersElapsed));
         fmt_time();
@@ -200,6 +278,8 @@ int main() {
   for (struct file_list_entry* f = files+SIZE(files); f-- > files; ) {
     f->h(f);
   }
+  fmt_cpu();
+  fmt_mem();
   fmt_time();
   setroot();
 
@@ -231,7 +311,7 @@ int main() {
   }
 
   // timer ----------------------------------------------------------
-  if ((minute_timer = timerfd_create(CLOCK_REALTIME, 0)) == -1) {
+  if ((timer_1min = timerfd_create(CLOCK_REALTIME, 0)) == -1) {
     ERR("timerfd_create");
     return 1;
   }
@@ -243,12 +323,12 @@ int main() {
   itspec.it_value.tv_sec +=
     itspec.it_interval.tv_sec - (tspec.tv_sec % itspec.it_interval.tv_sec);
   itspec.it_value.tv_nsec = 0;
-  if (timerfd_settime(minute_timer,TFD_TIMER_ABSTIME,&itspec,NULL) == -1) {
+  if (timerfd_settime(timer_1min,TFD_TIMER_ABSTIME,&itspec,NULL) == -1) {
     ERR("timerfd_settime");
     return 1;
   }
 
-  if (epoll_add(epoll,minute_timer) == -1) {
+  if (epoll_add(epoll,timer_1min) == -1) {
     ERR("epoll_add");
     return 1;
   }
